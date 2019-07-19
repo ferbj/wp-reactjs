6fdad9072c3a8f0e0648880db76c65bd5592bf0e
Fernando Balta <fernando@54solutions.com>
2019-07-19 15:23:13 -0500

    Integration Wordpress and Reactjs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "../src/components/header";
+import PostList from "./components/postList";
+import PostView from "../src/components/postView";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <section className="section container content">
+        <Route exact path="/" component={PostList} />
+        <Route path="/:slug" component={PostView} />
+      </section>
+    </div>
+  </Router>
+);
 
+//ReactDOM.render(<App />, document.getElementById("app"));
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
+import '../src/styles/style.css';
+import 'bulma/css/bulma.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
